23a9c43ed8dd2d516d7879ef8114bedc9c9923a6
paul <paalochoame@ittepic.edu.mx>
2023-10-26 17:38:01 -0700

    Agregar a favoritos

--- app/tab3/tab3.module.ts
+++ app/tab3/tab3.module.ts
@@ -1,20 +1,22 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { Component } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { Product } from '../models/product.model';
 
-import { Tab3PageRoutingModule } from './tab3-routing.module';
-
-@NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab3PageRoutingModule
-  ],
-  declarations: [Tab3Page]
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
 })
-export class Tab3PageModule {}
+export class Tab3Page {
+  favoriteItems: Product[];
+
+  constructor(private cartService: CartService) {
+    this.favoriteItems=this.cartService.getFavoriteItems();
+    console.log('Productos favoritos:', this.favoriteItems); 
+  }
+
+  removeFromFavorites(product: Product) {
+    this.cartService.removeFromFavorites(product);
+  }
+  
+}
